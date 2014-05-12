6afa437e0265a75a67610b1f2640dd4fec6afbb6
dfilatov <dfilatov@yandex-team.ru>
2014-05-12 18:20:20 +0400

    internal optimizations

--- modules.js
+++ modules.js
@@ -146,6 +146,21 @@ var undef,
                 }
 
                 var decls = [],
+                    onDeclResolved = function(_, error) {
+                        if(error) {
+                            cb(null, error);
+                            return;
+                        }
+
+                        if(!--unresolvedDepsCnt) {
+                            var exports = [],
+                                i = 0, decl;
+                            while(decl = decls[i++]) {
+                                exports.push(decl.exports);
+                            }
+                            cb(exports);
+                        }
+                    },
                     i = 0, len = unresolvedDepsCnt,
                     dep, decl;
 
@@ -163,33 +178,9 @@ var undef,
                         decl = dep;
                     }
 
-                    if(decl.state === DECL_STATES.IN_RESOLVING &&
-                            curOptions.trackCircularDependencies &&
-                            isDependenceCircular(decl, path)) {
-                        cb(null, buildCircularDependenceError(decl, path));
-                        return;
-                    }
-
                     decls.push(decl);
 
-                    startDeclResolving(
-                        decl,
-                        path,
-                        function(_, error) {
-                            if(error) {
-                                cb(null, error);
-                                return;
-                            }
-
-                            if(!--unresolvedDepsCnt) {
-                                var exports = [],
-                                    i = 0, decl;
-                                while(decl = decls[i++]) {
-                                    exports.push(decl.exports);
-                                }
-                                cb(exports);
-                            }
-                        });
+                    startDeclResolving(decl, path, onDeclResolved);
                 }
             },
 
@@ -198,13 +189,14 @@ var undef,
                     cb(decl.exports);
                     return;
                 }
-                else {
-                    decl.dependents.push(cb);
-                }
-
-                if(decl.state === DECL_STATES.IN_RESOLVING) {
+                else if(decl.state === DECL_STATES.IN_RESOLVING) {
+                    curOptions.trackCircularDependencies && isDependenceCircular(decl, path)?
+                        cb(null, buildCircularDependenceError(decl, path)) :
+                        decl.dependents.push(cb);
                     return;
                 }
+
+                decl.dependents.push(cb);
 
                 if(decl.prev && !curOptions.allowMultipleDeclarations) {
                     provideError(decl, buildMultipleDeclarationError(decl));
